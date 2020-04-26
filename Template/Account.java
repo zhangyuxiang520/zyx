5b748c81809a6c1db428fa8c1ec068e5a73255e7
zhangyuxiang520 <54885766+zhangyuxiang520@users.noreply.github.com>
2020-04-26 19:26:27 +0800

    Add files via upload

---
+++ Template/Account.java
@@ -0,0 +1,28 @@
+package Template;
+
+public abstract class Account {
+
+	public final double calculateInterest(){
+        double interestRate = doCalculateInterestRate();
+        String accountType = doCalculateAccountType();
+        double amount = calculateAmount(accountType);
+        return amount * interestRate;
+    }
+    /**
+     * 基本方法留给子类实现
+     */
+    protected abstract String doCalculateAccountType();
+    /**
+     * 基本方法留给子类实现
+     */
+    protected abstract double doCalculateInterestRate();
+    /**
+     * 基本方法，已经实现
+     */
+    private double calculateAmount(String accountType){
+        /**
+         * 省略相关的业务逻辑
+         */
+        return 7243.00;
+    }
+}
---
+++ Template/CDAccount.java
@@ -0,0 +1,14 @@
+package Template;
+
+public class CDAccount extends Account {
+
+	@Override
+    protected String doCalculateAccountType() {
+        return "Certificate of Deposite";
+    }
+
+    @Override
+    protected double doCalculateInterestRate() {
+        return 0.06;
+    }
+}
